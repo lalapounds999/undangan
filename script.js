1abb5660db1f0e8e2c77164ca8ffa142ee93954f
lalapounds999 <lalapounds11@gmail.com>
2026-02-10 08:05:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ function bukaUndangan(){
+  document.getElementById('cover').style.display='none';
+  document.getElementById('isi').classList.remove('hidden');
+  document.getElementById('musik').play();
+}
